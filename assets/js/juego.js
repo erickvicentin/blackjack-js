3b573ad05171138b7bb27415d0141f8d9a3782c6
Erick Emanuel Vicentin <erickvicentin.globant@latam.com>
2022-04-17 18:48:00 -0300

    Added all logic of game! ✔

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,7 +2,6 @@
 let deck = []
 let puntosJugador = 0
 let puntosPC = 0
-let playerName = ''
 
 /**
  * @name CARDS_TYPES
@@ -19,14 +18,27 @@ const cartasPC = document.querySelector('#pc-cards')
 const btnPedirCarta = document.querySelector('#req-btn')
 const btnPararJuego = document.querySelector('#stop-btn')
 const btnIniciarJuego = document.querySelector('#start-btn')
+const resultado = document.querySelector('.resultado')
+
+btnPararJuego.disabled = true
+btnPedirCarta.disabled = true
+
+const limpiarCartas = () => {
+    while (cartasJugador.lastChild) {
+        cartasJugador.removeChild(cartasJugador.lastChild)
+    }
+    while (cartasPC.lastChild) {
+        cartasPC.removeChild(cartasPC.lastChild)
+    }
+}
+
+const limpiarResultado = () => {
+    resultado.removeChild(resultado.lastChild)
+}
 
 const updateHTML = (puntosJugador, puntosPC) => {
     setPlayerPoints(puntosJugador)
     setPcPoints(puntosPC)
-}
-
-const setPlayerName = name => {
-    document.querySelectorAll('b')[0].innerText = name
 }
 
 const setPlayerPoints = points => {
@@ -83,23 +95,76 @@ btnPedirCarta.addEventListener('click', () => {
     imgCarta.classList.add('carta')
     cartasJugador.append(imgCarta)
 
-    if (puntosJugador > 21) {
+    if (puntosJugador >= 21) {
         btnPedirCarta.disabled = true
+        btnPararJuego.disabled = true
+        turnoComputadora(puntosJugador)
     }
 })
 
 btnIniciarJuego.addEventListener('click', () => {
     deck = crearDeck()
-    setPlayerName('Erick')
     puntosJugador = 0
     puntosPC = 0
-    document.getElementById('start-icon').innerText = ' Reiniciar juego'
-    document.getElementById('start-icon').className = 'fas fa-sync'
+    document.getElementById('start-btn').innerHTML = `
+        <div>
+            <i id='start-icon' class="fas fa-sync"></i>
+            Reiniciar
+        </div>
+        `     
     btnPedirCarta.disabled = false
-    while (cartasJugador.lastChild) {
-        cartasJugador.removeChild(cartasJugador.lastChild)
-    }
+    btnPararJuego.disabled = false
+    limpiarCartas()
+    limpiarResultado()
     updateHTML(puntosJugador, puntosPC)
 })
 
+btnPararJuego.addEventListener('click', () => {
+    btnPedirCarta.disabled = true
+    btnPararJuego.disabled = true
+    turnoComputadora(puntosJugador)
+})
+
 // turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta(deck)
+        puntosPC += valorCarta(carta)
+        updateHTML(puntosJugador, puntosPC)
+        const imgCarta = document.createElement('img')
+        imgCarta.src = CARD_PATH(carta)
+        imgCarta.classList.add('carta')
+        cartasPC.append(imgCarta)
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while ( puntosPC < puntosMinimos && puntosMinimos <= 21)
+    checkResultado(puntosMinimos, puntosPC)
+}
+
+const checkResultado = ( playerPoints, pcPoints ) => {
+    const resText = document.createElement('h1')
+    if( playerPoints === pcPoints ) {
+        resText.classList.add('empate')
+        resText.classList.add('result')
+        resText.innerText = 'ES UN EMPATE'
+        resultado.append(resText)
+    } else if ( playerPoints > 21 ) {
+        resText.classList.add('pc-win')
+        resText.classList.add('result')
+        resText.innerText = 'LA PC GANA'
+        resultado.append(resText)
+    } else if ( pcPoints > 21) {
+        resText.classList.add('result')
+        resText.classList.add('player-win')
+        resText.innerText = 'JUGADOR GANA'
+        resultado.append(resText)
+    } else {
+        resText.classList.add('result')
+        resText.classList.add('pc-win')
+        resText.innerText = 'LA PC GANA'
+        resultado.append(resText)
+    }
+}
